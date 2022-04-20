29e6177e906738b1e4ba0be5811aed2fcf9ed525
mohith-venkata <mohithreddy.venkata@gmail.com>
2022-04-20 20:03:19 +0530

    grammer correction

--- BinarySearchTopic/InfinteArray.java
+++ BinarySearchTopic/InfinteArray.java
@@ -8,15 +8,14 @@ public class InfinteArray{
         }
 
 
-        //finds the chuck(start,end) where does the target lies and pass (start,end) to binarySearch().
+        //finds the chunk(start,end) where does the target lies and pass (start,end) to binarySearch().
         public static int ans(int[] arr,int target){
-            //first find the range
-            //first start with a box of size 2
+            //first start with a box of size 2.
             int start=0;
             int end=1;
-            while(target>arr[end]){     //untill arr[end]>target, we grow the size of our chunk exponential(double it).
-                int temp=end+1;          //save the end value in temp.so,we will use it to create next start.
-                end=end+(end-start+1)*2;     //double the chunk size: end=prev_end+(sizeofbox*2)   
+            while(target>arr[end]){     //untill arr[end]>target, we grow the size of our chunk exponentially(double it).
+                int temp=end+1;          //save the end value in temp.so,we will use it to create next_start.
+                end=end+(end-start+1)*2;     //double the chunk size: "end=prev_end+(sizeofbox*2)" 
                 start=temp;
             }
             int ans=binarySearch(arr, target, start, end);
@@ -47,5 +46,6 @@ NOTE:
 1.We cannot create a infinte array instead we just consider not to use arr.length
 2.We cannot search target without start and end in binary search. 
 3.We will search chunk by chunk in an array. 
+4.Chunk sizes will grow like starts with 2,4,8,16..... grows expontentially(double) untill arr[end]>target.
 20 APR 2022
 */
